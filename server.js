cb3a02656891e74b4fd4009262ad6263079f3034
Jordan Thaler <jordanpthaler@gmail.com>
2017-10-28 11:46:08 -0500

    updates to server.js

---
+++ server.js
@@ -0,0 +1,61 @@
+// Dependencies
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Arrays
+var reservation = [];
+var waitlist = [];
+
+// // Test
+// app.get("/", function(req, res) {
+//   res.send("Huzzah!");
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// App listening
+app.listen(PORT, function() {
+    console.log("App is listening on PORT " + PORT);
+});
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Adding to Reservation/Waitlist
+app.post("/api/new", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newReservation = req.body;
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+
+    characters.push(newReservation);
+
+    res.json(newReservation);
+});
+
+// Get lists
+app.get("/api/:list", function(req, res) {
+    var chosen = req.params.list;
+
+    if (chosen === "reservation") {
+        res.json(reservation);
+
+    else {
+    	res.json(waitlist);
+        }
+    };
+}); 
